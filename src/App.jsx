4b53899cf74e30f6a98e4e3b3b13c746619cbd1e
Narayan Reddy <narayanreddy355@gmail.com>
2025-04-24 18:44:51 +0530

    Add Shop Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import React from 'react'
-import Nav from './components/Nav'
 import LocomotiveScroll from 'locomotive-scroll';
-import Page01 from './section2/Page01';
-import Page02 from './section2/Page02';
-import Page03 from './section2/Page03';
-import Page04 from './section2/Page04';
-import Page05 from './section2/Page05';
-import Page06 from './section2/Page06';
-import Page07 from './section2/Page07';
-
-
+import Shop from './section2/Shop';
 
 const App = () => {
 const locomotiveScroll = new LocomotiveScroll();
 
   return (
     <>
-   <Page01/>
-   <Page02/>
-  <Page03/>
-  <Page04/>
-  <Page05/>
-  <Page07/>
-  <Page06/>
+    <Shop/>
     </>
   )
 }
---
+++ src/section2/Shop.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+const Shop = () => {
+  const [changeColor,steChangeColor]=useState(false)
+  console.log(changeColor);
+  
+  return (
+    <div className='min-h-screen flex bg-black max-[599px]:flex-col text-white'>
+        <div className=' max-[599px]:w-[100%] w-[50%] flex flex-col justify-end max-[599px]:items-center max-[599px]:my-20 max-[599px]:p-4  max-[1024px]:p-7 p-14 gap-y-4'>
+          <h1 className='font-[font-1] max-[1024px]:text-5xl text-8xl'>Premium</h1>
+          <h1 className='font-[font-2] max-[1024px]:text-5xl text-8xl max-[599px]:text-center '>PREMADE <br />
+          SERVICE</h1>
+          <p className='font-[font-2] text-[1.3vw] text-gray-400 max-[599px]:w-[90%] max-[599px]:text-base w-[80%] max-[599px]:text-center'>Launch your Website within days - cheap and with no headaches</p>
+          <div className="flex flex-wrap max-[599px]:justify-center gap-4 lg:gap-x-10 group">
+  {/* Shop Sites Button */}
+  <div className="flex justify-center">
+    <button className="bg-[#D9D9D9] hover:bg-black hover:text-white px-9 py-2.5 rounded-full flex items-center  gap-4 max-[599px]:px-14 text-black ">
+      <img
+        className="w-6 max-[599px]:w-7 text-black group-hover:invert"
+        src="./src/assets/Images/cartola_Logo.png"
+        alt="Cartola Logo"
+      />
+      <span className="text-base">Shop Sites</span>
+    </button>
+  </div>
+
+  {/* Explore Feature Button */}
+  <div className="flex justify-center">
+    <button className="bg-black text-white px-9 py-2.5 rounded-full flex items-center gap-4 border border-white group-hover:bg-white group-hover:text-black">
+      <img
+        className="w-6 max-[599px]:w-7 group-hover:invert"
+        src="./src/assets/Images/Iron_Logo.png"
+        alt="Iron Logo"
+      />
+      <span className="text-base">Explore Feature</span>
+    </button>
+  </div>
+</div>
+
+        </div>
+        <div className='max-[599px]:w-full w-[50%]  h-screen max-[599px]:h-[70%] overflow-hidden '>
+          <img className='object-cover max-[599px]:w-full max-[599px]:h-full h-[100%] object-center w-full' src="./src/assets/Images/bgImg7.png" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Shop
